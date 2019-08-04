89b30d6a713d364fb492802077ea05892f6e3b63
Zé Pedro Matos <revolty@gmail.com>
2019-08-04 01:41:05 +0100

    User Login

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -4,7 +4,7 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAI,
+  LOGIN_FAIL,
   LOGOUT,
   CLEAR_ERRORS
 } from "../types";
@@ -19,6 +19,7 @@ export default (state, action) => {
         user: action.payload
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
         ...state,
@@ -28,6 +29,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return {
         ...state,
